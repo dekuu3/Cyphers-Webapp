bebd5b8c590c22a3cdfea8e80ecc4be22d67d3e0
dekuu3 <davidpireslazaro@gmai.com>
2019-04-14 22:40:19 +0100

    Updated navbar css

--- cypherscw/routes/index.js
+++ cypherscw/routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
 router.get('/cyphers', function(req ,res ,next) {
 	res.render('cyphers', {title:'Cyphers'});
+});
+
+router.get('/contact', function(req ,res ,next) {
+	res.render('cyphers', {title:'Contact'});
+});
+
+router.get('/about', function(req ,res ,next) {
+	res.render('cyphers', {title:'About'});
 });
 
 module.exports = router;
