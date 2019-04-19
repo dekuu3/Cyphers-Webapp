ddac76fead1c609564796b5fcf52cbbdda1b6414
dekuu3 <davidpireslazaro@gmai.com>
2019-04-19 10:06:54 +0100

    Added validation for when sending messages

--- cypherscw/public/javascripts/msg.js
+++ cypherscw/public/javascripts/msg.js
@@ -21,6 +21,32 @@
 )();
 
 function sendmsg(){
+	document.getElementById("error_text").value = null;
+	var e = document.getElementById("dd");
+	var dropdownoption = e.options[e.selectedIndex].value;
+
+	if (document.getElementById("key").value.length == 0 && dropdownoption == 3)
+     {
+		var errormsg1 = "Key is empty!";
+  		var splitString = errormsg1.split("");
+		document.getElementById("error_text").value = splitString.join("");
+		return;
+	}
+	var key = filterKey(document.getElementById("key").value);
+	if(key.length == 0 && dropdownoption == 3)
+     {
+		var errormsg2 = "Key has no letters!";
+		var splitString = errormsg2.split("");
+  		document.getElementById("error_text").value = splitString.join("");
+		return;
+	}
+	if(document.getElementById("msgusername").value.length == 0 )
+     {
+		var errormsg2 = "The username field is empty!";
+		var splitString = errormsg2.split("");
+  		document.getElementById("error_text").value = splitString.join("");
+		return;
+	}
 
   var usermessage = {"username": document.getElementById("msgusername").value,
   "message": document.getElementById("message").value };
